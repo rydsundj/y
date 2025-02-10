e7f01b698b8801499bf8dafc3b2f1025d3890adb
rydsundj <123332000+rydsundj@users.noreply.github.com>
2025-02-10 18:23:17 +0100

    Update next.config.mjs

--- project/y/next.config.mjs
+++ project/y/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Exclude node built-in modules from the client-side bundle
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: false,
+        worker_threads: false,
+      };
+    }
+    return config;
+  },
+};
